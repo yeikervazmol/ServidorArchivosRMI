77c2bed7f066ba60cde41d05817505036ef63b6f
yeiker_7 <yeikervazmol@gmail.com>
2014-01-16 19:18:12 -0430

    Documentacion de AutenticacionImpl.java.

--- ServidorAU/src/AutenticacionImpl.java
+++ ServidorAU/src/AutenticacionImpl.java
@@ -6,7 +6,7 @@
 * Archivo: AutenticacionImpl.java
 *
 * Descripcion: Implementacion de la interfaz para el programa 
-* principal del servidor de autentificacion (Autenticacion.java).
+* principal del servidor de autenticacion (Autenticacion.java).
 */
 
 import java.io.BufferedReader;
@@ -19,21 +19,54 @@ public class AutenticacionImpl
         extends        java.rmi.server.UnicastRemoteObject 
         implements Autenticacion {
         
-        /**
-         * 
+        /*
+         * Version de la implementacion de la interfaz Autenticacion
          */
         private static final long serialVersionUID = 1L;
+        
+        /*
+         * Variable que contiene el nombre del archivo que posee
+         * 	los nombres y claves de los clientes permitidos en el
+         * 	servidor de archivos. 
+         */
         public static String archivoUsu = "";
         
+        /*
+         * AutenticacionImpl:
+         * 	Se encarga de asignar a la variable el nombre del 
+         * 	archivo.
+         * 
+         * @param 	Es el nombre del archivo que contiene los nombres
+         * 			y claves de los usuarios con acceso al servidor.
+         * @throws 	java.rmi.RemoteException.
+         * @return 	Objeto de AutenticacionImpl. 
+         */
         public AutenticacionImpl(String archivo)
         throws java.rmi.RemoteException {
                 super();
                 archivoUsu = archivo;
         }
 
-        
+        /*
+         * autenticarUsuario:
+         * 	Se encarga de verificar que el nombre y clave pasados
+         * 	como argumento estan en el archivo de usuarios con
+         * 	acceso al servidor de archivos.
+         * 
+         * @param 	Nombre del usuario que se quiere autenticar.
+         * @param 	Clave del usuario que se quiere autenticar.
+         * @throws 	java.rmi.RemoteException.
+         * @return 	True si el usuario con su clave se encuentra en
+         * 			el archivo de usuarios permitidos, False en caso
+         * 			contrario.
+         */
         public Boolean autenticarUsuario(String nombre, String clave)
         throws java.rmi.RemoteException {
+        	
+        		/*
+        		 * Variables necesarias para hacer la lectura del
+        		 * 	archivo.
+        		 */
                 BufferedReader usuarios = null;
                 String[] datosUsuario = { "" , ""};
                 String linea = "";
@@ -41,24 +74,57 @@ public class AutenticacionImpl
                 String claveLinea = "";
                 
                 try {
+                	
+                		/*
+                		 * Se abre el archivo y se recorre linea por
+                		 * 	linea buscando los nombres y las claves.
+                		 */
+                		
                         usuarios = new BufferedReader(new FileReader(new File(archivoUsu)));
                         linea = usuarios.readLine();
                         while ( linea != null) {
                                 datosUsuario = linea.split(":");
                                 nombreLinea = datosUsuario[0];
                                 claveLinea = datosUsuario[1];
+                                
+                                /*
+                                 * Caso en el que coincide el nombre
+                                 * 	y la clave.
+                                 */
                                 if (nombre.equals(nombreLinea) && clave.equals(claveLinea)) {
+                                		usuarios.close();
                                         return true;
                                 }
                                 linea = usuarios.readLine();
                         }
+                        usuarios.close();
+                      
                         
-                } catch (FileNotFoundException e) {
+                        
+                }
+                
+                /*
+                 * Excepcion que protege al programa de que no
+                 * 	encuentre el archivo solicitado.
+                 */
+                catch (FileNotFoundException e) {
                         System.out.println("Archivo no encontrado.");
                         System.exit(0);
-                } catch (IOException e) {
+                } 
+                
+                /*
+                 * Excepcion que protege al programa de que haya
+                 * 	algun problema leyendo el archivo.
+                 */
+                catch (IOException e) {
                         e.printStackTrace();
                 }
+                
+                /*
+                 * Caso en el que el nombre y clave del usuario
+                 * no coincide con ninguno de los registrados
+                 * en el archivo.
+                 */
                 return false;
         }
 
